f6c40906b45f1826339fac5cdf272a8873c1a07b
jwkk1 <dog107.2@gmail.com>
2023-04-05 16:55:02 +0900

    feat: http => dio 변경

--- lib/store/searchListStore.dart
+++ lib/store/searchListStore.dart
@@ -1,20 +1,25 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
 
 
 class SearchListStore extends ChangeNotifier {
   var searchList = [];
+  var dio = Dio();
 
 
   getList() async {
     try{
-      var result = await http.get(Uri.parse('${dotenv.env['BASE_URL']}'));
-      var result2 = jsonDecode(result.body);
-      searchList = result2['items'];
-      notifyListeners();
+      var params = {'locale' : 'kr','category' : 'c4', 'page' : '2'};
+      var response = await dio.get('${dotenv.env['BASE_URL']}', queryParameters: params);
+      // var response = await http.get(Uri.parse('${dotenv.env['BASE_URL']}'));
+      if(response.statusCode == 200){
+        searchList = response.data['items'];
+        notifyListeners();
+      }
     }catch(e){
       print(e);
     }
