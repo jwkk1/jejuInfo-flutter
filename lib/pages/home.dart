3ed65d8abad6f6375ae1f29ff3843f2b5f8bc782
jwkk1 <dog107.2@gmail.com>
2023-04-17 14:53:18 +0900

    feat: NestedScrollView 추가 appbar 제거

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,123 +13,145 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   var category = 'c1';
+  var scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Jeju Info'),
-        actions: [IconButton(onPressed: (){}, icon: Icon(Icons.my_library_add))],
-      ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(5, 30, 5, 5),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              pinned: false,
+              floating: true,
+              toolbarHeight: 150,
+              backgroundColor: Colors.white,
+              title: Text('Jeju Info'),
+              flexibleSpace: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                      child: Image.network(
+                        'https://www.agoda.com/wp-content/uploads/2020/04/Jeju-Island-hotels-things-to-do-in-Jeju-Island-South-Korea.jpg',
+                        fit: BoxFit.cover,
+                      ))
+                ],
+              ),
+            ),
+            // 변경사
+          ];
+        },
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(2, 0, 2, 2),
+            child: Column(
               children: [
-                GestureDetector(
-                  onTap: (){
-                    context.read<SearchListStore>().changeCategory('c1');
-                    setState((){
-                      category = 'c1';
-                    });
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: (){
+                        context.read<SearchListStore>().changeCategory('c1');
+                        setState((){
+                          category = 'c1';
+                        });
 
-                  },
-                  child: Column(
-                    children: [
-                      Text('관광지', style: TextStyle(fontSize: 20, color: category == 'c1'
-                          ? Colors.black
-                          : Colors.grey,)),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                        height: 2,
-                        width: 50,
-                        color: category == 'c1'
-                            ? Colors.orange
-                            : Color.fromRGBO(255, 255, 255, 0.0),
+                      },
+                      child: Column(
+                        children: [
+                          Text('관광지', style: TextStyle(fontSize: 20, color: category == 'c1'
+                              ? Colors.black
+                              : Colors.grey,)),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            height: 2,
+                            width: 50,
+                            color: category == 'c1'
+                                ? Colors.lightBlue
+                                : Color.fromRGBO(255, 255, 255, 0.0),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: (){
-                    context.read<SearchListStore>().changeCategory('c2');
-                    setState((){
-                      category = 'c2';
-                    });
-                  },
-                  child: Column(
-                    children: [
-                      Text('쇼핑', style: TextStyle(fontSize: 20, color: category == 'c2'
-                          ? Colors.black
-                          : Colors.grey,)),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                        height: 2,
-                        width: 50,
-                        color: category == 'c2'
-                            ? Colors.orange
-                            : Color.fromRGBO(255, 255, 255, 0.0)
-                        ,
+                    ),
+                    GestureDetector(
+                      onTap: (){
+                        context.read<SearchListStore>().changeCategory('c2');
+                        setState((){
+                          category = 'c2';
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text('쇼핑', style: TextStyle(fontSize: 20, color: category == 'c2'
+                              ? Colors.black
+                              : Colors.grey,)),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            height: 2,
+                            width: 50,
+                            color: category == 'c2'
+                                ? Colors.lightBlue
+                                : Color.fromRGBO(255, 255, 255, 0.0)
+                            ,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: (){
-                    context.read<SearchListStore>().changeCategory('c3');
-                    setState((){
-                      category = 'c3';
-                    });
-                  },
-                  child: Column(
-                    children: [
-                      Text('숙박', style: TextStyle(fontSize: 20, color: category == 'c3'
-                          ? Colors.black
-                          : Colors.grey,)),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                        height: 2,
-                        width: 50,
-                        color: category == 'c3'
-                            ? Colors.orange
-                            : Color.fromRGBO(255, 255, 255, 0.0),
+                    ),
+                    GestureDetector(
+                      onTap: (){
+                        context.read<SearchListStore>().changeCategory('c3');
+                        setState((){
+                          category = 'c3';
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text('숙박', style: TextStyle(fontSize: 20, color: category == 'c3'
+                              ? Colors.black
+                              : Colors.grey,)),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            height: 2,
+                            width: 50,
+                            color: category == 'c3'
+                                ? Colors.lightBlue
+                                : Color.fromRGBO(255, 255, 255, 0.0),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: (){
-                    context.read<SearchListStore>().changeCategory('c4');
-                    setState((){
-                      category = 'c4';
-                    });
-                  },
-                  child: Column(
-                    children: [
-                      Text('음식점', style: TextStyle(fontSize: 20, color: category == 'c4'
-                          ? Colors.black
-                          : Colors.grey,)),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                        height: 2,
-                        width: 50,
-                        color: category == 'c4'
-                            ? Colors.orange
-                            : Color.fromRGBO(255, 255, 255, 0.0),
+                    ),
+                    GestureDetector(
+                      onTap: (){
+                        context.read<SearchListStore>().changeCategory('c4');
+                        setState((){
+                          category = 'c4';
+                        });
+                      },
+                      child: Column(
+                        children: [
+                          Text('음식점', style: TextStyle(fontSize: 20, color: category == 'c4'
+                              ? Colors.black
+                              : Colors.grey,)),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            height: 2,
+                            width: 50,
+                            color: category == 'c4'
+                                ? Colors.lightBlue
+                                : Color.fromRGBO(255, 255, 255, 0.0),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
+                // 상단 카테고리 영역
+                SizedBox(
+                  height: 15,
+              ),
+                Expanded(child: SearchList())
               ],
             ),
-            // 상단 카테고리 영역
-            SizedBox(
-              height: 15,
           ),
-            Expanded(child: SearchList())
-          ],
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -143,4 +165,5 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
 
